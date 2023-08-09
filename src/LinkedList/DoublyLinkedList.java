37393f25b233805cf63517dbc22fa8d9d99a4ced
Tripurari Singh <learn.with.tripurari@gmail.com>
2023-08-09 22:50:48 +0530

    Improved the insertAtIndex() method

--- src/LinkedList/DoublyLinkedList.java
+++ src/LinkedList/DoublyLinkedList.java
@@ -19,17 +19,28 @@ public class DoublyLinkedList {
     public static void main(String[] args) {
 
 
+        insertAtIndex(6, 0);
          insertEnd(1);
          insertEnd(2);
          insertEnd(3);
 
          insertAtIndex(4,2);
+         insertAtIndex(5, 0);
 
          Node temp = head;
 
          while (temp != null) {
              System.out.println(temp.data);
              temp = temp.next;
+         }
+
+        System.out.println("    // Tail");
+
+        Node tailTemp = tail;
+
+         while (tailTemp != null) {
+             System.out.println(tailTemp.data);
+             tailTemp = tailTemp.prev;
          }
 
 
@@ -55,32 +66,52 @@ public class DoublyLinkedList {
 
         Node newNode = new Node(data);
 
-        if(head == null) {
-            System.out.println("List is empty!");
-            return;
+        if(index == 0) {
+
+             newNode.next = head;
+
+             if (head != null) {
+                 head.prev = newNode;
+             }
+
+             head = newNode;
+
+             if (tail == null) {
+                 tail = newNode;
+             }
         }
+        else {
 
-        Node currentNode = head;
-        int currentIndex = 0;
+            Node currentNode = head;
+            int currentIndex = 0;
 
-        while (currentNode !=  null && currentIndex < index - 1) {
+           while (currentNode !=  null && currentIndex < index - 1) {
             currentNode = currentNode.next;
             currentIndex++;
+          }
+
+           if (currentNode == null) {
+               System.out.println("Invalid Index");
+               return;
+           }
+
+           newNode.next = currentNode.next;
+
+            if(currentNode.next != null ) {
+                currentNode.next.prev = newNode;
+            }
+
+           newNode.prev = currentNode;
+
+           currentNode.next = newNode;
+
+
+
+           if (newNode.next == null) {
+               tail = newNode;
+           }
+
         }
-
-        if (currentNode == null) {
-            System.out.println("Invalid index");
-            return;
-        }
-
-
-       newNode.next = currentNode.next;
-        newNode.prev = currentNode;
-
-        if(currentNode.next != null) {
-            currentNode.next.prev = newNode;
-        }
-        currentNode.next = newNode;
 
     }
 
