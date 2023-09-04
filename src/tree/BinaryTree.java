d1ca777aca3c75134baab705de2afd8101fc80ad
Tripurari Singh <learn.with.tripurari@gmail.com>
2023-09-04 22:19:13 +0530

    insert function implemented

--- src/tree/BinaryTree.java
+++ src/tree/BinaryTree.java
@@ -19,19 +19,41 @@ public class BinaryTree {
 
     public static void main(String[] args) {
 
-      TreeNode n1  =  new TreeNode(1);
-      TreeNode n2  =  new TreeNode(2);
-      TreeNode n3  =  new TreeNode(3);
-      TreeNode n4  =  new TreeNode(4);
+//      TreeNode n1  =  new TreeNode(1);
+//      TreeNode n2  =  new TreeNode(2);
+//      TreeNode n3  =  new TreeNode(3);
+//      TreeNode n4  =  new TreeNode(4);
+//
+//
+//      root = n1;
+//      n1.left = n2;
+//      n1.right = n3;
+//      n2.left = n4;
+//
+//      System.out.println("Height of the Tree " + height(root));
 
+      root = insert(root, 5);
+      insert(root, 3);
+      insert(root, 8);
+      insert(root, 2);
+      insert(root, 4);
 
-      root = n1;
-      n1.left = n2;
-      n1.right = n3;
-      n2.left = n4;
+    }
 
-      System.out.println("Height of the Tree " + height(root));
+    static TreeNode insert(TreeNode node, int data) {
 
+        if (node == null) {
+            return  new TreeNode(data);
+        }
+
+        if (data < node.data) {
+           node.left  = insert(node.left, data);
+        }
+        else if (data > node.data) {
+           node.right  = insert(node.right, data);
+        }
+
+        return node;
     }
 
     static int height(TreeNode node) {
