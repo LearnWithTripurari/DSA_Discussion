e98fd5d4bac38874f9b25112b248b6cfe4dd5f43
Tripurari Singh <learn.with.tripurari@gmail.com>
2023-09-21 22:12:42 +0530

    Solution ArrayOfProducts

--- src/Array2/ArrayOfProducts.java
+++ src/Array2/ArrayOfProducts.java
@@ -34,4 +34,25 @@ public class ArrayOfProducts {
         }
         return res;
     }
+
+//    public int[] productExceptSelf(int[] nums) {
+//
+//        int[] res = new int[nums.length];
+//
+//        for(int i = 0; i < nums.length; i++) {
+//
+//            int product = 1;
+//
+//            for(int j =0; j < nums.length; j++) {
+//
+//                if(i != j) {
+//                    product *= nums[j];
+//                }
+//            }
+//
+//            res[i] = product;
+//        }
+//
+//        return res;
+//    }
 }
